7aa66f9c6f08158cdf7bf9a408738a820e5c0763
chenyuan <80950159@qq.com>
2018-01-04 11:10:13 +0800

    日志服务升级

--- LibAndroidLogger/src/main/java/com/hianzuo/logger/Log.java
+++ LibAndroidLogger/src/main/java/com/hianzuo/logger/Log.java
@@ -2,6 +2,9 @@ package com.hianzuo.logger;
 
 import android.os.RemoteException;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.text.SimpleDateFormat;
 
 /**
@@ -22,7 +25,6 @@ public class Log {
         LogServiceHelper.append("E/" + tag + ": " + message);
     }
 
-
     public static void v(String tag, String message) {
         android.util.Log.v(tag, message);
         LogServiceHelper.append("V/" + tag + ": " + message);
@@ -30,12 +32,12 @@ public class Log {
 
     public static void i(String tag, String message) {
         android.util.Log.i(tag, message);
-        LogServiceHelper.append( "I/" + tag + ": " + message);
+        LogServiceHelper.append("I/" + tag + ": " + message);
     }
 
     public static void d(String tag, String message) {
         android.util.Log.d(tag, message);
-        LogServiceHelper.append( "D/" + tag + ": " + message);
+        LogServiceHelper.append("D/" + tag + ": " + message);
     }
 
     public static void w(String tag, String message) {
@@ -43,8 +45,16 @@ public class Log {
         LogServiceHelper.append("W/" + tag + ": " + message);
     }
 
-    public static String getStackTraceString(Throwable tr) {
-        return android.util.Log.getStackTraceString(tr);
+    public static void w(String tag, String message, Throwable e) {
+        android.util.Log.w(tag, message, e);
+        String throwMsg = getThrowMessage(e);
+        LogServiceHelper.append("W/" + tag + ": " + message + " Throwable:" + throwMsg);
+    }
+
+    public static void e(String tag, String message, Throwable e) {
+        android.util.Log.d(tag, message, e);
+        String throwMsg = getThrowMessage(e);
+        LogServiceHelper.append("E/" + tag + ": " + message + " Throwable:" + throwMsg);
     }
 
     public static void println(int priority, String tag, String message) {
@@ -52,18 +62,55 @@ public class Log {
         LogServiceHelper.append("PrintLn/" + tag + ": " + message);
     }
 
-    public static void w(String tag, String msg, Throwable e) {
-        android.util.Log.w(tag, msg, e);
+    public static String getStackTraceString(Throwable tr) {
+        return android.util.Log.getStackTraceString(tr);
     }
 
-    public static void e(String tag, String message, Throwable e) {
-        android.util.Log.d(tag, message, e);
-        String throwMsg = e.getMessage();
-        if (null == throwMsg) throwMsg = e.getClass().getSimpleName();
-        LogServiceHelper.append("E/" + tag + ": " + message + " Throwable:" + throwMsg);
+    private static String getThrowMessage(Throwable e) {
+        String throwMsg = "null";
+        if (null != e) {
+            throwMsg = e.getMessage();
+            if (null == throwMsg) {
+                throwMsg = e.getClass().getSimpleName();
+            }
+        }
+        return throwMsg;
+    }
+
+    public static void eThrowable(String tag, Throwable e) {
+        if (null == e) {
+            return;
+        }
+        String lines = toString(e);
+        eLines(tag, lines);
+    }
+
+    public static void eLines(String tag, String lines) {
+        if (null != lines && lines.length() > 0) {
+            String[] ss = lines.split("\n");
+            if (ss.length > 0) {
+                for (String s : ss) {
+                    Log.e(tag, s.replace("\t", "        "));
+                }
+                LogServiceHelper.flush();
+            }
+        }
     }
 
 
+    private static String toString(Throwable e) {
+        StringWriter writer = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(writer);
+        e.printStackTrace(printWriter);
+        printWriter.flush();
+        String errMsg = writer.toString();
+        printWriter.close();
+        try {
+            writer.close();
+        } catch (IOException ignored) {
+        }
+        return errMsg;
+    }
 
     public static void flush() {
         LogServiceHelper.flush();
